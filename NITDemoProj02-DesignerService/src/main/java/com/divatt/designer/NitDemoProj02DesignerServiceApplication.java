88c14839a4f7358fa3bfc686918978a5f759d99a
nitaachal20 <aachal@nitsolution.in>
2022-07-07 20:21:06 +0530

    New Project

---
+++ NITDemoProj02-DesignerService/src/main/java/com/divatt/designer/NitDemoProj02DesignerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.divatt.designer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitDemoProj02DesignerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitDemoProj02DesignerServiceApplication.class, args);
+	}
+
+}
