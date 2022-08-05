3c3bdf807b25fbc17084660e0b685754338d777d
nitaachal20 <aachal@nitsolution.in>
2022-08-05 09:44:51 +0530

    VARIABLE ADDED

--- NITDemoProj02-DesignerService/src/main/java/com/divatt/designer/demo.java
+++ NITDemoProj02-DesignerService/src/main/java/com/divatt/designer/demo.java
@@ -3,8 +3,8 @@ package com.divatt.designer;
 public class demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x = 20;
+		System.out.println(x);
 	}
 
 }
